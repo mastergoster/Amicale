80b913101214e8cecbeae2fa2c22ad9ac44b6000
Richard <benjamin.siskoo.gmail.com>
2019-11-09 15:29:22 +0100

    correction de la taille des images sur l'accueil

--- view/menu.php
+++ view/menu.php
@@ -1,5 +1,5 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light" style="height:40px" >
-        <a class="navbar-brand"><img src="assets/images/logo_amicale.png" alt="Logo Amicale" class="img-circle img-responsive"></a>
+        <a class="navbar-brand"><img class="logosize" src="assets/images/logo_amicale.png" alt="Logo Amicale" class="img-circle img-responsive"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
