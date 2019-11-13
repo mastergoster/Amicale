58bc4edc4189e4f343647fca9b05f3dfd8d97a24
Richard <benjamin.siskoo.gmail.com>
2019-11-13 15:40:12 +0100

    correction affichage texte footer admin

--- administration/view/footer.php
+++ administration/view/footer.php
@@ -1,30 +1,22 @@
 </div>
-            <footer class="footer">
-                <div class="container">
-                    <div class="row">
-                        <div class="">
-                            Contact :
-                            <!--Facebook-->
-                            <button type="button" class="btn btn-fb"><i class="fab fa-facebook-f"></i></button>
-                            <!--Email-->
-                            <button type="button" class="btn btn-email"><i class="fas fa-envelope"></i></button>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+    <footer class="footer col-md-12">
+        <div class="row">
+            <div class="col-md-8"></div>
+
+                <h4>Contact : Tél : 04 70 02 78 49 - Email : <a href="mailto:?to=amicale@ch-montlucon.fr"> amicale@ch-montlucon.fr </a></h4> 
+                <!--Facebook-->
+                <!--<button type="button" class="btn btn-fb"><i class="fab fa-facebook-f"></i></button>
+                Email
+                <button type="button" class="btn btn-email"><i class="fas fa-envelope"></i></button>-->
+            </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>        
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script> 
-        <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script> 
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-        <script src="assets/js/script.js"></script>
-        <script>
-                $('#myDatepicker').datetimepicker({
-	            format: 'DD-MM-YYYY HH:mm'
-                });
-        </script>
+    </footer>
+
+    <script>
+            $('#myDatepicker').datetimepicker({
+            format: 'DD-MM-YYYY HH:mm'
+            });
+    </script>
 
     </body>
 </html>
--- view/footer.php
+++ view/footer.php
@@ -1,22 +1,22 @@
-        
-        </div>
-        <footer class="footer col-md-12">
-            <div class="row">
-                <div class="col-md-8"></div>
+</div>
+    <footer class="footer col-md-12">
+        <div class="row">
+            <div class="col-md-8"></div>
 
-                    <h4>Contact : Tél : 04 70 02 78 49 - Email : <a href="mailto:?to=amicale@ch-montlucon.fr"> Contacter l'Amicale </a></h4> 
-                    <!--Facebook-->
-                    <!--<button type="button" class="btn btn-fb"><i class="fab fa-facebook-f"></i></button>
-                    Email
-                    <button type="button" class="btn btn-email"><i class="fas fa-envelope"></i></button>-->
-                </div>
+                <h4>Contact : Tél : 04 70 02 78 49 - Email : <a href="mailto:?to=amicale@ch-montlucon.fr"> Contacter l'Amicale </a></h4> 
+                <!--Facebook-->
+                <!--<button type="button" class="btn btn-fb"><i class="fab fa-facebook-f"></i></button>
+                Email
+                <button type="button" class="btn btn-email"><i class="fas fa-envelope"></i></button>-->
             </div>
-        </footer>
-        
-        <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        </div>
+    </footer>
+    
+    
+    <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 
-        
-        <script type="text/javascript" src="assets/js/script.js"></script>
+    
+    <script type="text/javascript" src="assets/js/script.js"></script>
 </body>
 </html>
