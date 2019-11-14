b6057153cc93e2016d337b9103e8970015082b2f
Richard <benjamin.siskoo.gmail.com>
2019-11-14 09:26:40 +0100

    remplacement dans le footer des textes téléphone et email par des icônes

--- view/footer.php
+++ view/footer.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-8"></div>
 
-                <h4>Contact : Tél : 04 70 02 78 49 - Email : <a href="mailto:?to=amicale@ch-montlucon.fr"> amicale@ch-montlucon.fr </a></h4> 
+                <h4>Nous contacter par : <i class="fas fa-phone-square-alt"></i> : 04 70 02 78 49 - <i class="fas fa-envelope"></i> : <a href="mailto:?to=amicale@ch-montlucon.fr"> amicale@ch-montlucon.fr </a></h4> 
                 <!--Facebook-->
                 <!--<button type="button" class="btn btn-fb"><i class="fab fa-facebook-f"></i></button>
                 Email
